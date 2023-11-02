6f012a7d3ff56836349ccdbd4d79815308133bfb
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-11-02 17:12:12 +0200

    create router

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
-console.log("Hello i am server");
-console.log("Hi");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+const myRouter = require("./routes/goalsRouters.js");
+
+const PORT = process.env.PORT || 7777;
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/goals", myRouter);
+
+goals;
+
+app.listen(PORT, () => {
+  console.log(`Server started on the port ${PORT}`);
+});
