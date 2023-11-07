70e6ea37e37f6b033bc5e4b1253433d260eb8e1b
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-11-07 07:45:04 +0200

    create server.js and goalsRouters.js

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
-const myRouter = require("./routes/goalsRouters.js");
-
 const PORT = process.env.PORT || 7777;
+const myRouter = require("./routes/goalsRouters.js");
 
 const app = express();
 
-app.use(cors());
-
 app.use("/api/goals", myRouter);
-
-goals;
 
 app.listen(PORT, () => {
   console.log(`Server started on the port ${PORT}`);
